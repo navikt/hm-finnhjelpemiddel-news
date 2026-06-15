061d7112af223aa615d9d66b13e157d1fbd43041
Andreas.Danielsen.Fageraas <andreas.danielsen.fageraas@nav.no>
2026-06-15 13:41:00 +0200

    added alivecontroller

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hm-finnhjelpemiddel-news
+
+## Running hm-finnhjelpemiddel-news locally:
+
+```
+cd hm-finnhjelpemiddel-news
+docker-compose up -d
+
+export DB_DRIVER=org.postgresql.Driver
+export DB_JDBC_URL=jdbc:postgresql://localhost:5435/finnhjelpemiddelnews
+export SERVER_PORT=1338
+
+./gradlew build run
+```
---
+++ src/main/kotlin/no/nav/hm/finnhjelpemiddelnews/internal/AliveController.kt
@@ -0,0 +1,16 @@
+package no.nav.hm.finnhjelpemiddelnews.internal;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Controller("/internal")
+@Hidden
+class AliveController() {
+
+    @Get("/isAlive")
+    fun alive() = "ALIVE"
+
+    @Get("/isReady")
+    fun ready() =  "OK"
+}
